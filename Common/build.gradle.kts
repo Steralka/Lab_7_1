458dd7a879f0130e70da8ead8c79dcc1e8ae06ba
Steralka <kirsan1805@yandex.ru>
2025-05-31 21:48:31 +0300

    .
    .

---
+++ Common/build.gradle.kts
@@ -0,0 +1,23 @@
+plugins {
+    id("java")
+}
+
+group = "labs.lab7.common"
+version = "1.0-SNAPSHOT"
+
+tasks.withType<JavaCompile> {
+    options.encoding = "UTF-8"
+}
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    testImplementation(platform("org.junit:junit-bom:5.10.0"))
+    testImplementation("org.junit.jupiter:junit-jupiter")
+}
+
+tasks.test {
+    useJUnitPlatform()
+}
